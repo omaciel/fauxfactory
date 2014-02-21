dea7f40bd9c800ff0e93cd1a7b4f54887058cdc1
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-02-21 06:56:59 -0300

    Add license and description to setup definition

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 
+with open('LICENSE') as file:
+    license = file.read()
+
 with open('README.rst') as file:
     long_description = file.read()
 
@@ -10,5 +13,7 @@ setup(
     author='Og Maciel',
     author_email='omaciel@redhat.com',
     packages=['fauxfactory'],
+    description='Generates random data for your tests.',
     long_description=long_description,
+    license=license,
 )
