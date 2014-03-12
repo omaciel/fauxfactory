e0f38d676f4d04bd102086bc5401b984fcf2e0b1
Og B. Maciel <omaciel@ogmaciel.com>
2014-03-12 10:36:42 -0400

    Tests for UUID generator.
    This should handle issue #5.

---
+++ tests/test_uuids.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for UUID generator
+"""
+
+from fauxfactory import FauxFactory
+
+import unittest
+
+
+class TestUUID(unittest.TestCase):
+    """
+    Test UUID generator
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Instantiate our factory object
+        """
+
+        cls.factory = FauxFactory()
+
+    def test_generate_uuid_1(self):
+        """
+        @Test: Create a random UUID value
+        @Feature: UUID Generator
+        @Assert: A random UUID value is generated
+        """
+
+        for turn in xrange(100):
+            result = self.factory.generate_uuid()
+            self.assertGreater(len(result), 0,
+                               "A valid UUID value was not generated.")
