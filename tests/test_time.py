6a1d15f81f649968642c1fa7a4c91f0706711770
Og B. Maciel <omaciel@ogmaciel.com>
2014-03-12 10:38:21 -0400

    Tests for Time generator.
    Should handle issue #3.

---
+++ tests/test_time.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for Time generator
+"""
+
+from fauxfactory import FauxFactory
+
+import unittest
+import datetime
+
+
+class TestTime(unittest.TestCase):
+    """
+    Test Time generator
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Instantiate our factory object
+        """
+
+        cls.factory = FauxFactory()
+
+    def test_generate_uuid_1(self):
+        """
+        @Test: Create a random UUID value
+        @Feature: UUID Generator
+        @Assert: A random UUID value is generated
+        """
+
+        for turn in xrange(100):
+            result = self.factory.generate_time()
+            self.assertIsInstance(result, datetime.time,
+                                  "A valid Time value was not generated.")
