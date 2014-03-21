25384672ec7fb26376159b5c81dd853d732583da
Og B. Maciel <omaciel@ogmaciel.com>
2014-03-21 11:36:47 -0400

    Added classifiers to setup.py.

--- setup.py
+++ setup.py
@@ -8,12 +8,20 @@ with open('README.rst') as file:
 
 setup(
     name='fauxfactory',
-    version='0.1.2',
-    url='https://github.com/omaciel/fauxfactory',
-    author='Og Maciel',
-    author_email='omaciel@redhat.com',
-    packages=['fauxfactory'],
     description='Generates random data for your tests.',
     long_description=long_description,
+    version='0.1.2',
+    author='Og Maciel',
+    author_email='omaciel@redhat.com',
+    url='https://github.com/omaciel/fauxfactory',
+    packages=['fauxfactory'],
     license=license,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ]
 )
