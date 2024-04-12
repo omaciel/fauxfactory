d3fca4b7afb7d45cf329326c1c18f811972be5bc
Og B. Maciel <omaciel@ogmaciel.com>
2024-04-11 20:17:40 -0400

    Looking into potential doc build issue.

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "FauxFactory"
-copyright = "2014-2017, Og Maciel"  # pylint:disable=redefined-builtin
+copyright = "2014-2024, Og Maciel"  # pylint:disable=redefined-builtin
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
